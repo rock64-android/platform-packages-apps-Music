9fd335c7a2972ac5acd4bbd21139cfbfae586681
Hans Yang <yhx@rock-chips.com>
2016-08-13 16:42:12 +0800

    force Music compile to priv-app to prevent error if pms multithread scan is on
    Change-Id: I209e3e5bd8aca7e6b7464b26cb41f5605c5c880a

--- Android.mk
+++ Android.mk
@@ -7,6 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
 	src/com/android/music/IMediaPlaybackService.aidl
 
 LOCAL_PACKAGE_NAME := Music
+LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_SDK_VERSION := current
 
